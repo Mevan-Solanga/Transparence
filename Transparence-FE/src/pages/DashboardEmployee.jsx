246cbaeab0e48f130d46ca0821a8130a69b12d56
Samudra Perera <samudrapup@gmail.com>
2024-05-18 13:47:22 -0400

    Added logout

--- Transparence-FE/src/pages/DashboardEmployee.jsx
+++ Transparence-FE/src/pages/DashboardEmployee.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useAuth } from "../context/auth-context";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 const DashboardEmployee = () => {
-  return <div>DashboardEmployee</div>;
+  const { logout } = useAuth();
+  const navigate = useNavigate(); // Initialize navigate
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/"); // Navigate to the login page
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Dashboard Employee</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 };
 
 export default DashboardEmployee;
