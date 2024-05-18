b4d629663a5cd407d0921908b0ed9971dc1bc13f
Samudra Perera <samudrapup@gmail.com>
2024-05-18 15:05:13 -0400

    Fetching users for Dashboard

--- Transparence-FE/src/pages/DashboardEmployee.jsx
+++ Transparence-FE/src/pages/DashboardEmployee.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/auth-context";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../utils/firebase";
 
 const DashboardEmployee = () => {
   const { logout } = useAuth();
   const navigate = useNavigate(); // Initialize navigate
+
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    const fetchCompanies = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "companies"));
+        const companiesList = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setCompanies(companiesList);
+      } catch (error) {
+        console.error("Error fetching companies:", error);
+      }
+    };
+
+    fetchCompanies();
+  }, []);
+
+  console.log(companies);
 
   const handleLogout = async () => {
     try {
@@ -18,6 +41,9 @@ const DashboardEmployee = () => {
   return (
     <div>
       <h1>Dashboard Employee</h1>
+      {companies.map((e) => (
+        <div>{e.companyName}</div>
+      ))}
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
