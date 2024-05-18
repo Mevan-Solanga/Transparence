dcf1dc59da6b94532e6d5031f97586034fc7c8c0
Samudra Perera <samudrapup@gmail.com>
2024-05-18 00:35:32 -0400

    Login and Sigup

--- Transparence-FE/src/pages/Login.jsx
+++ Transparence-FE/src/pages/Login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useAuth } from "../context/auth-context";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await login(email, password);
+    } catch (error) {
+      console.error("LOGIN ERROR");
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
 };
 
 export default Login;
--- Transparence-FE/src/pages/Signup.jsx
+++ Transparence-FE/src/pages/Signup.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useAuth } from "../context/auth-context";
+import { useState } from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
+  const { signup } = useAuth();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await signup(email, password, role);
+      router.push("/"); // Navigate to dashboard after signup
+    } catch (error) {
+      console.error("ERROR SIGNING UP");
+    }
+  };
+
+  <form onSubmit={handleSubmit}>
+    <input
+      type="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      required
+      placeholder="Email"
+    />
+    <input
+      type="password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      required
+      placeholder="Password"
+    />
+    <select value={role} onChange={(e) => setRole(e.target.value)} required>
+      <option value="">Select Role</option>
+      <option value="Company">Doctor</option>
+      <option value="Applicant">Patient</option>
+    </select>
+    <button type="submit">Sign Up</button>
+  </form>;
 };
 
 export default Signup;
